75bae1c96b3f4b94af8de3e14fd37d8e59393c21
Becky Marshall <becky.marshall@id.iit.edu>
2017-04-06 15:36:04 -0500

    created moveAmount variables for each target amount; removed ‘default’ color style for 5th box

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ boxes[0].style.backgroundColor = "green";
 boxes[1].style.backgroundColor = "purple";
 boxes[2].style.backgroundColor = "#191919";
 boxes[3].style.backgroundColor = "blanchedAlmond";
-boxes[4].style.backgroundColor = "default";
 
 // Variable to hold the current left margin.
 var leftMargin0 = 0;
@@ -15,7 +14,11 @@ var leftMargin3 = 0;
 var topMargin = 0;
 
 // Variable used to add to the left margin.
-var moveAmount = 5;
+var moveAmount5 = 5;
+var moveAmount10 = 10;
+var moveAmount15 = 15;
+var moveAmount20 = 20;
+var moveAmount25 = 25;
 
 // Function that adds moveAmount to leftMargin then appends leftMargin
 // to the first box's margin left. Basically moving the box to the right.
@@ -24,41 +27,31 @@ function box0Clicked() {
     // 'console.log()' only displays in the browser console.
     // It's useful for you, the developer, to debug your code.
     console.log("Hey, box clicked!");
-    leftMargin0 += moveAmount;
+    leftMargin0 += moveAmount5;
     boxes[0].style.marginLeft = leftMargin0 + "px";
 }
 
 function box1Clicked() {
     console.log("Hey, box clicked!");
-    leftMargin1 += moveAmount;
-    leftMargin1 += moveAmount;
+    leftMargin1 += moveAmount10;
     boxes[1].style.marginLeft = leftMargin1 + "px";
 }
 
 function box2Clicked() {
     console.log("Hey, box clicked!");
-    leftMargin2 += moveAmount;
-    leftMargin2 += moveAmount;
-    leftMargin2 += moveAmount;
+    leftMargin2 += moveAmount15;
     boxes[2].style.marginLeft = leftMargin2 + "px";
 }
 
 function box3Clicked() {
     console.log("Hey, box clicked!");
-    leftMargin3 += moveAmount;
-    leftMargin3 += moveAmount;
-    leftMargin3 += moveAmount;
-    leftMargin3 += moveAmount;
+    leftMargin3 += moveAmount20;
     boxes[3].style.marginLeft = leftMargin3 + "px";
 }
 
 function box4Clicked() {
     console.log("Hey, box clicked!");
-    topMargin += moveAmount;
-    topMargin += moveAmount;
-    topMargin += moveAmount;
-    topMargin += moveAmount;
-    topMargin += moveAmount;
+    topMargin += moveAmount25;
     boxes[4].style.marginTop = topMargin + "px";
 }
 
